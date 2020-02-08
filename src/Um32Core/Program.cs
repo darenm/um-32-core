d27e67a4f06c81cd1f75e6ce969c7ed655bb269f
Daren May <daren.may@gmail.com>
2020-02-08 09:53:45 -0700

    Adding code to project

---
+++ src/Um32Core/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Um32Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Register a = 3;
+            Register b = 3;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(a+b);
+        }
+    }
+}
---
+++ src/Um32Core/Um32.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Um32Core
+{
+    public class Um32Core
+    {
+
+    }
+}
